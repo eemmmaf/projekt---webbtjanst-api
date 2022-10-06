bba41775c5fce25269159120a2192c7e1db370a1
Emma Forslund <emfo2102@student.miun.se>
2022-10-06 13:44:53 +0200

    Skapat en model för produkter

--- projektapi/routes/api.php
+++ projektapi/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//Om ett anrop kommer används Controllern ProductController, som sköter anropet
+Route::resource('product', ProductController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
