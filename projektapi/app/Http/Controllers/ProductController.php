4e15d0c61069484b49778221c0c204bd6f95da9c
Emma Forslund <emfo2102@student.miun.se>
2022-10-07 14:07:49 +0200

    Skapat nya funktioner

--- projektapi/app/Http/Controllers/ProductController.php
+++ projektapi/app/Http/Controllers/ProductController.php
@@ -24,19 +24,16 @@ class ProductController extends Controller
             'name' => 'required',
             'description' => 'required',
             'price' => 'required',
-            'quantity' => 'required',
-            'category_id' => 'required'
+            'quantity' => 'required'
         ]);
 
-        //Lägger till och returnerar den nya podcasten som har skapats
+        //Lägger till och returnerar den nya Produkten som har skapats
         return Products::create($request->all());
 
         return response()->json([
             'Produkt har lagts till'
         ], 200);
     }
-
-
 
     //Lägg till kategori
     public function addCategory(Request $request)
@@ -55,5 +52,66 @@ class ProductController extends Controller
     {
         //Returnerar alla lagrade kategorier
         return Category::all();
+    }
+
+    //Funktion för att hämta en produkt utifrån produktens id
+    public function getProductById($id)
+    {
+
+        //Hämtar Produkt utifrån dess id
+        $product = Products::find($id);
+
+        //Kontroll om variabeln $product inte är lika med null
+        if ($product != null) {
+            //Returnerar Produkten
+            return $product;
+        } else {
+            //Skriver ut felmeddelande och felkoden 404 om variabeln $product är null
+            return response()->json([
+                'Produkten kunde inte hittas!'
+            ], 404);
+        }
+    }
+
+    public function updateProduct(Request $request, $id)
+    {
+        //Hämtar product utifrån dess id och sparar i variabeln $product
+        $product = Products::find($id);
+
+        //Validerar och kontrollerar att allt är ifyllt. Krav på att alla fält ska vara ifyllda
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'quantity' => 'required'
+        ]);
+
+        //Kontroll om variabeln $product inte är lika med null
+        if ($product != null) {
+            //Uppdaterar 
+            $product->update($request->all());
+            //Returnerar det uppdaterade innehållet
+            return $product;
+        } else {
+            //Returnerar ett felmeddelande och felkod om producten inte kunde hittas
+            return response()->json(['producten kunde inte hittas'], 404);
+        }
+    }
+
+    //Funktion för att ta bort en produkt
+    public function destroy($id)
+    {
+        //Hämtar product utifrån dess ID
+        $product = Products::find($id);
+
+        //Kontroll om producten inte är lika med null
+        if ($product != null) {
+            //Tar bort producten
+            $product->delete();
+            //Returnerar 
+            return response()->json(['Produkten har tagits bort']);
+        } else {
+            return response()->json(['Producten kunde inte hittas'], 404);
+        }
     }
 }
