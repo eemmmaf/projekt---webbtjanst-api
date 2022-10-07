8a84563b1742282a84380b3f7eee8e5338ca4b34
Emma Forslund <emfo2102@student.miun.se>
2022-10-07 15:16:48 +0200

    Skapat routes för inloggning, utloggning, regist

--- projektapi/routes/api.php
+++ projektapi/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,19 +19,24 @@ use App\Http\Controllers\ProductController;
 */
 
 //Post-routes
-Route::post('/addproduct/{id}', [ProductController::class, 'addProduct']); //Lägger till en produkt
-Route::post('/addcategory', [ProductController::class, 'addCategory']); //Lägger till en kategori
+Route::post('/addproduct/{id}', [ProductController::class, 'addProduct'])->middleware('auth:sanctum'); //Lägger till en produkt
+Route::post('/addcategory', [ProductController::class, 'addCategory'])->middleware('auth:sanctum');  //Lägger till en kategori
 //Get-routes
-Route::get('/getproducts', [ProductController::class, 'getProducts']); //Hämtar alla produkter
-Route::get('/getcategories', [ProductController::class, 'getCategories']); //Hämtar alla kategorier
-Route::get('/getproductbyid/{id}', [ProductController::class, 'getProductById']); //Hämtar enskild produkt
+Route::get('/getproducts', [ProductController::class, 'getProducts'])->middleware('auth:sanctum'); //Hämtar alla produkter
+Route::get('/getcategories', [ProductController::class, 'getCategories'])->middleware('auth:sanctum');  //Hämtar alla kategorier
+Route::get('/getproductbyid/{id}', [ProductController::class, 'getProductById'])->middleware('auth:sanctum'); //Hämtar enskild produkt
+Route::get('/products/search/{name}', [ProductController::class, 'searchProduct'])->middleware('auth:sanctum'); //Söka efter produkt efter namn
 //Put-routes
-Route::put('/updateproduct/{id}', [ProductController::class, 'updateProduct']); //Lägger till en produkt
+Route::put('/updateproduct/{id}', [ProductController::class, 'updateProduct'])->middleware('auth:sanctum');  //Lägger till en produkt
 //Delete-route
-Route::delete('/deleteproduct/{id}', [ProductController::class, 'destroy']); //Lägger till en produkt
+Route::delete('/deleteproduct/{id}', [ProductController::class, 'destroy'])->middleware('auth:sanctum');  //Lägger till en produkt
 
-
-
+//Route för att registrera (Publik)
+Route::post('/register', [AuthController::class, 'register']);
+//Route för att logga in
+Route::post('/login', [AuthController::class, 'login']);
+//Route för att logga ut
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
